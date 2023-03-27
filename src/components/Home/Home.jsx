ef7c47765a536c210ac894ef198b5d6e692fbc69
brokaX <broka_exogene@live.fr>
2023-03-28 00:08:28 +0300

    Home Page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,12 +3,21 @@ import './Home.css'
 import logo from '../../assets/logo.png'
 function Home() {
   return (
-    <div className='home-page'>
-      <img className='home-logo' src={logo} alt="BrokaX Logo" />
-      <h3>I am Abbes Touidjine</h3>
-      <p>I like building web applications</p>
-    </div>
-  )
+    <>
+      <section className='home-page'>
+        <img className='home-logo' src={logo} alt='BrokaX Logo' />
+        <h3>I am Abbes Touidjine</h3>
+        <p>I like building web applications</p>
+      </section>
+      <section className='portfolio-section'>
+        <h1>Recent Projects</h1>
+        <p>I like building web applications</p>
+        <div className='cards-container'>
+          
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default Home
